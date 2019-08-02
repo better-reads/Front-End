5840310ebc9dee1bc02cd55b2a7c051870e3fdb6
Marcus Kwan <kwan.marcus@gmail.com>
2019-08-03 08:01:15 +1000

    linked marketing page to footer logo

--- better-reads-app/src/components/Footer.js
+++ better-reads-app/src/components/Footer.js
@@ -5,17 +5,19 @@ import { Image } from "semantic-ui-react";
 function Footer(props) {
   return (
     <footer className="footer">
-        <div className="logo">
-        <Link to="/"><Image
+      <div className="logo">
+        <a href="https://better-reads-marketing.netlify.com/">
+          <Image
             className="logo__img"
             alt="betterreads logo"
             src={require("../imgs/br-logo.png")}
-          /></Link>
-        </div>
+          />
+        </a>
+      </div>
 
-        <div className="nav-links">
-          <p className="copyright">&copy;2019</p>
-        </div>
+      <div className="nav-links">
+        <p className="copyright">&copy;2019</p>
+      </div>
     </footer>
   );
 }
